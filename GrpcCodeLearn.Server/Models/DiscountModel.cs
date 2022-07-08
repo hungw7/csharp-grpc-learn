d4a34f6909b4fdc462b30670ac4a90c187426447
Hung Nguyen <gaughegomth01@gmail.com>
2022-07-09 02:19:47 +0700

    update

---
+++ GrpcCodeLearn.Server/Models/DiscountModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GrpcCodeLearn.Server.Models;
+
+public class DiscountModel
+{
+	public string Id { get; set; } = null!;
+	public double Amount { get; set; }
+}
---
+++ GrpcCodeLearn.Server/Services/DiscountService.cs
@@ -0,0 +1,30 @@
+﻿#nullable disable
+
+using Grpc.Core;
+using GrpcCodeLearn.Server.Models;
+
+namespace GrpcCodeLearn.Server.Services;
+
+public class DiscountService : Discount.DiscountBase
+{
+	private readonly List<DiscountModel> _discounts = new List<DiscountModel>
+	{
+		new DiscountModel { Id = "1", Amount = 40 },
+		new DiscountModel { Id = "2", Amount = 50 },
+		new DiscountModel { Id = "3", Amount = 60 },
+	};
+
+	public override Task<GetDiscountResponse> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+	{
+		var response = _discounts.FirstOrDefault(x => x.Id == request.DiscountId);
+
+		if (response is null)
+			return Task.FromResult(new GetDiscountResponse());
+		
+		return Task.FromResult(new GetDiscountResponse()
+		{
+			Id = response!.Id,
+			Amount = response.Amount
+		});
+	}
+}
